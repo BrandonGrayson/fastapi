3b1d993072ae1efd66181d81363b61beca9b7df8
Brandon Grayson <brandon.grayson@pihealth.ai>
2022-07-27 00:22:53 -0400

    conn string updated with env variables

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Prolific1@localhost/fastapi3"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.fastapi_database_username}:{settings.fastapi_database_password}@{settings.fastapi_database_port}/fastapi3"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
