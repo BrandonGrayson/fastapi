129f83c6731ee3c93c2b2c3a0e0e14b87f44e9e6
Brandon Grayson <brandon.grayson@pihealth.ai>
2022-08-10 23:11:14 -0400

    create_user func created

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,8 +2,17 @@ from http.client import HTTPException
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from typing import Union
+
+
 
 router = APIRouter()
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 @router.post('/login')
 def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
@@ -15,5 +24,15 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
 
     if not utils.verify_password(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+    
+    def create_user(data: dict, expires_delta: Union[timedelta, None] = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utc() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return encoded_jwt
 
     return {"token": "example token"}
